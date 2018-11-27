24547277f57139cab1c653004f2689dc46ae0591
Hector Pablo <pablo.cardenas>
2018-11-27 00:47:38 -0700

    Renombrado carpeta 'modules' por 'components'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Login from './modules/Login';
+import Login from './components/Login';
 
 class App extends Component {
   render() {
